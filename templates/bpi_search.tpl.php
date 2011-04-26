5b6f2b26eefa374eccb06c4ad80ada67a7957816
AM <am@inleadmedia.com>
2011-04-26 17:36:11 +0300

    case 5280 - BPI Search view.

---
+++ templates/bpi_search.tpl.php
@@ -0,0 +1,14 @@
+<?php
+
+echo drupal_render(drupal_get_form('bpi_search_filters'));
+
+?>
+
+<div id="bpi_nodes">
+<?php
+
+echo bpi_well_nodes();
+
+?>
+</div>
+<div class="clear"></div>
---
+++ templates/bpi_well_node.tpl.php
@@ -0,0 +1,27 @@
+<div class="well_node">
+  <div class="node_col">
+    <p class="node_title"><?=$title?><input type="checkbox" <?php if($read) : ?>checked="checked"<?php ;endif?> /></p>
+    <p class="node_description"><?=$description?></p>
+    <div class="node_controls">
+      <div class="syndicate">Syndiker</div>
+      <div>Preview</div>
+      <div>Kommentar</div>
+      <!-- @todo : add rating stars -->
+      <span>(<?=$rated?>)</span>
+     </div>
+  </div>
+  <div class="node_col">
+    <p>Dato: <?=$date?></p>
+    <p>Forfatter: <?=$author?></p>
+    <p>Kategori: <?=$category?></p>
+    <p>Ibrug: <?=$syndicated?></p>
+  </div>
+  <div class="node_col">
+    <p>Lix: <?=$lix?></p>
+    <p>Bibliotek: <?=$library?></p>
+    <p>Kanal: <?=$channel?></p>
+    <p class="details"><?=l('Vis alle detaljer >>', '#')?></p>
+  </div>
+  <div class="clear"></div>
+  <hr />
+</div>
