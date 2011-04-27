49c83cd8ecde3d76a0c8046587fc3314e54c9ccf
AM <am@inleadmedia.com>
2011-04-27 12:39:37 +0300

    case 5280 - Added search form to search page.

--- templates/bpi_search.tpl.php
+++ templates/bpi_search.tpl.php
@@ -1,14 +1,23 @@
-<?php
-
-echo drupal_render(drupal_get_form('bpi_search_filters'));
-
-?>
-
-<div id="bpi_nodes">
-<?php
-
-echo bpi_well_nodes();
-
-?>
+<div class="page_form">
+  <?php
+  
+  echo drupal_render(drupal_get_form('bpi_search_form'));
+  
+  ?>
 </div>
-<div class="clear"></div>
+<div class="page_result">
+  <?php
+
+  echo drupal_render(drupal_get_form('bpi_search_filters'));
+
+  ?>
+
+  <div id="bpi_nodes">
+  <?php
+
+  echo bpi_well_nodes();
+
+  ?>
+  </div>
+  <div class="clear"></div>
+</div>
