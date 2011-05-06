5a2a5b27672ae41614b07143c331274cd453b218
AM <am@inlead.dk>
2011-05-06 14:32:32 +0300

    Updated layout according to FB cases.

--- templates/bpi_flag_dropdown.tpl.php
+++ templates/bpi_flag_dropdown.tpl.php
@@ -1,20 +1,20 @@
 <div class="dropdown">
   <div class="rate rate_0">
-    <img src="<?=BPI_PATH . '/img/flag_black.jpg'?>" class="flag" width="16" height="16" alt="" />&nbsp;
-    <img src="<?=BPI_PATH . '/img/dd_arrow.gif'?>" class="arrow" width="16" height="16" alt="" />
+    <img src="/<?=BPI_PATH . '/img/flag_black.jpg'?>" class="flag" width="16" height="16" alt="" />&nbsp;
+    <img src="/<?=BPI_PATH . '/img/dd_arrow.gif'?>" class="arrow" width="16" height="16" alt="" />
     <div class="clear"></div>
   </div>
   <div class="rate_items">
     <div class="rate rate_1">
-      <img src="<?=BPI_PATH . '/img/flag_green.jpg'?>" class="flag" width="16" height="16" alt="" />
+      <img src="/<?=BPI_PATH . '/img/flag_green.jpg'?>" class="flag" width="16" height="16" alt="" />
       <div class="clear"></div>
     </div>
     <div class="rate rate_2">
-      <img src="<?=BPI_PATH . '/img/flag_yellow.jpg'?>" class="flag" width="16" height="16" alt="" />
+      <img src="/<?=BPI_PATH . '/img/flag_yellow.jpg'?>" class="flag" width="16" height="16" alt="" />
       <div class="clear"></div>
     </div>
     <div class="rate rate_3">
-      <img src="<?=BPI_PATH . '/img/flag_red.jpg'?>" class="flag" width="16" height="16" alt="" />
+      <img src="/<?=BPI_PATH . '/img/flag_red.jpg'?>" class="flag" width="16" height="16" alt="" />
       <div class="clear"></div>
     </div>
   </div>
--- templates/bpi_well_node.tpl.php
+++ templates/bpi_well_node.tpl.php
@@ -1,16 +1,24 @@
 <div class="well_node">
   <div class="node_col">
-    <p class="node_title"><?=$title?><input type="checkbox" <?php if($read) : ?>checked="checked"<?php ;endif?> /></p>
-    <p><label>Add to favorites: </label><?=theme('bpi_flag_dropdown')?></p>
+    <h1 class="node_title" style="float: left"><?=$title?></h1>
+    <p style="float: right;"><input type="checkbox" <?php if($read) : ?>checked="checked"<?php ;endif?> /></p>
+    <div class="clear"></div>
+    <?=theme('bpi_flag_dropdown')?>
     <div class="clear"></div>
     <p class="node_description"><?=$description?></p>
     <div class="node_controls">
-      <div class="syndicate">Syndiker</div>
-      <div>Preview</div>
+      <div class="syndicate"><?=l('Syndicate', 'admin/content/ajax/node/1/syndicate', array('attributes' => array('class' => array('use-ajax'))))?></div>
+      <div><?=l('Preview', 'node/1')?></div>
       <div>Kommentar</div>
-      <!-- @todo : add rating stars -->
+    </div>
+    <div class="rating">
+      <img src="<?=BPI_PATH . '/img/star-off.png'?>" width="12" height="12" alt="" />
+      <img src="<?=BPI_PATH . '/img/star-off.png'?>" width="12" height="12" alt="" />
+      <img src="<?=BPI_PATH . '/img/star-off.png'?>" width="12" height="12" alt="" />
+      <img src="<?=BPI_PATH . '/img/star-off.png'?>" width="12" height="12" alt="" />
+      <img src="<?=BPI_PATH . '/img/star-off.png'?>" width="12" height="12" alt="" />
       <span>(<?=$rated?>)</span>
-     </div>
+    </div>
   </div>
   <div class="node_col">
     <p>Dato: <?=$date?></p>
@@ -22,7 +30,7 @@
     <p>Lix: <?=$lix?></p>
     <p>Bibliotek: <?=$library?></p>
     <p>Kanal: <?=$channel?></p>
-    <p class="details"><?=/*l('Vis alle detaljer >>', '#')*/'Vis alle detaljer >>'?></p>
+    <p class="details"><?=l('Vis alle detaljer >>', 'node/1')?></p>
   </div>
   <div class="clear"></div>
   <hr />
