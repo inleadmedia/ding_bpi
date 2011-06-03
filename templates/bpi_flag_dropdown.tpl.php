72549497439a75c834005d1fcf91c7cffcb50860
AnatolyMuntean <am@inleadmedia.com>
2011-06-03 16:22:44 -0400

    case 5361 - Added flag dropdown.

--- templates/bpi_flag_dropdown.tpl.php
+++ templates/bpi_flag_dropdown.tpl.php
@@ -1,21 +1,15 @@
+<?php
+
+$flags = array('flag_black', 'flag_green', 'flag_yellow', 'flag_red');
+
+?>
+
 <div class="dropdown">
-  <div class="rate rate_0">
-    <img src="/<?=BPI_PATH . '/img/flag_black.jpg'?>" class="flag" width="16" height="16" alt="" />&nbsp;
-    <img src="/<?=BPI_PATH . '/img/dd_arrow.gif'?>" class="arrow" width="16" height="16" alt="" />
-    <div class="clear"></div>
-  </div>
-  <div class="rate_items">
-    <div class="rate rate_1">
-      <img src="/<?=BPI_PATH . '/img/flag_green.jpg'?>" class="flag" width="16" height="16" alt="" />
-      <div class="clear"></div>
-    </div>
-    <div class="rate rate_2">
-      <img src="/<?=BPI_PATH . '/img/flag_yellow.jpg'?>" class="flag" width="16" height="16" alt="" />
-      <div class="clear"></div>
-    </div>
-    <div class="rate rate_3">
-      <img src="/<?=BPI_PATH . '/img/flag_red.jpg'?>" class="flag" width="16" height="16" alt="" />
-      <div class="clear"></div>
-    </div>
-  </div>
+  <select name="favorites">
+    <?php
+    foreach($flags as $k=>$v) {
+      echo '<option value="' . ($k + 1) . '_'. mt_rand() .'" ' . ((($k + 1) == $selected) ? 'selected="selected"' : '') .'>' . $k . '</option>';
+    }
+    ?>
+  </select>
 </div>
