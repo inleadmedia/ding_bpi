3ac5b087caf81c34d542a3c0a6f78519d92c2dcb
AnatolyMuntean <am@inleadmedia.com>
2011-06-08 12:14:05 -0400

    case 5358 - Filter above favorites grid & Remove option.

--- templates/bpi_favorites.tpl.php
+++ templates/bpi_favorites.tpl.php
@@ -1,13 +1,17 @@
-<h3>Show only</h3>
+<div class="filters">
+  <h3>Show only</h3>
+  <div class="flag_filter">
+    <?php echo drupal_render($form['show_red']); ?>
+  </div>
+  <div class="flag_filter">
+    <?php echo drupal_render($form['show_yellow']); ?>
+  </div>
+  <div class="flag_filter">
+    <?php echo drupal_render($form['show_green']); ?>
+  </div>
+  <div class="clear"></div>
+</div>
 <?php
-
-echo theme('bpi_flag_dropdown', array('selected' => 0));
-
-?>
-
-<br /><br />
-
-<?
 
 echo drupal_render_children($form);
 
