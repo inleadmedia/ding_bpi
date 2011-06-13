0c23d160a379b41ab6e95415e3acfc963dedc0a3
AnatolyMuntean <am@inleadmedia.com>
2011-06-13 16:40:50 -0400

    case 5351 - Corrected display of favorites section in node lists.

--- templates/bpi_flag_dropdown.tpl.php
+++ templates/bpi_flag_dropdown.tpl.php
@@ -4,6 +4,8 @@
  *
  */
 
+$favorites = unserialize($selected);
+
 ?>
 
 <div class="dropdown">
@@ -11,22 +13,22 @@
     <div class="flags">
       <div class="flag">
         <img src="/<?php echo BPI_PATH;?>/img/flag_black.jpg" width="16" height="16" alt="" />
-        <input name="flagged[]" type="checkbox" value="0" />
+        <input name="flagged[]" type="checkbox" value="0" <?php echo (($favorites[0]) ? 'checked="checked"' : ''); ?> />
         <div class="clear"></div>
       </div>
       <div class="flag">
         <img src="/<?php echo BPI_PATH;?>/img/flag_red.jpg" width="16" height="16" alt="" />
-        <input name="flagged[]" type="checkbox" value="1" />
+        <input name="flagged[]" type="checkbox" value="1" <?php echo (($favorites[1]) ? 'checked="checked"' : ''); ?> />
         <div class="clear"></div>
       </div>
       <div class="flag">
         <img src="/<?php echo BPI_PATH;?>/img/flag_yellow.jpg" width="16" height="16" alt="" />
-        <input name="flagged[]" type="checkbox" value="2" />
+        <input name="flagged[]" type="checkbox" value="2" <?php echo (($favorites[2]) ? 'checked="checked"' : ''); ?> />
         <div class="clear"></div>
       </div>
       <div class="flag">
         <img src="/<?php echo BPI_PATH;?>/img/flag_green.jpg" width="16" height="16" alt="" />
-        <input name="flagged[]" type="checkbox" value="3" />
+        <input name="flagged[]" type="checkbox" value="3" <?php echo (($favorites[3]) ? 'checked="checked"' : ''); ?> />
         <div class="clear"></div>
       </div>
     </div>
