d3a0be8de20fb6fadb8bd1d7c2885a587ed9e1e5
Dragan <di@inlead.dk>
2011-07-07 09:46:32 +0300

    case 5674 - Added a new template for subscription with an extra column. This is a temporary dirty solution

---
+++ templates/bpi_subscriptions.tpl.php
@@ -0,0 +1,119 @@
+
+<div>
+
+<div class="form-item form-type-checkboxes form-item-subscribe-audience">
+  <label for="edit-subscribe-audience">Target audience: </label>
+ <div class="form-checkboxes" id="edit-subscribe-audience">
+
+<div class="bpi_left form-item form-type-checkbox form-item-subscribe-audience-0">
+ <input type="checkbox" class="form-checkbox" value="0" name="subscribe_audience[0]" id="edit-subscribe-audience-0">  <label for="edit-subscribe-audience-0" class="option">Adults </label>
+</div>
+<div class="bpi_left form-item form-type-checkbox form-item-subscribe-audience-0">
+ <input type="checkbox" class="form-checkbox" value="0" name="subscribe_audience[0]" id="edit-subscribe-audience-0">  <label for="edit-subscribe-audience-0" class="option">Auto update </label>
+</div>
+
+<div class="clear"></div>
+
+<div class="bpi_left form-item form-type-checkbox form-item-subscribe-audience-1">
+ <input type="checkbox" class="form-checkbox" value="1" name="subscribe_audience[1]" id="edit-subscribe-audience-1">  <label for="edit-subscribe-audience-1" class="option">Seniors </label>
+</div>
+
+<div class="bpi_left form-item form-type-checkbox form-item-subscribe-audience-0">
+ <input type="checkbox" class="form-checkbox" value="0" name="subscribe_audience[0]" id="edit-subscribe-audience-0">  <label for="edit-subscribe-audience-0" class="option">Auto update </label>
+</div>
+
+<div class="clear"></div>
+
+
+
+<div class="bpi_left form-item form-type-checkbox form-item-subscribe-audience-2">
+ <input type="checkbox" class="form-checkbox" value="2" name="subscribe_audience[2]" id="edit-subscribe-audience-2">  <label for="edit-subscribe-audience-2" class="option">Children </label>
+
+</div>
+
+<div class="bpi_left form-item form-type-checkbox form-item-subscribe-audience-0">
+ <input type="checkbox" class="form-checkbox" value="0" name="subscribe_audience[0]" id="edit-subscribe-audience-0">  <label for="edit-subscribe-audience-0" class="option">Auto update </label>
+</div>
+
+<div class="clear"></div>
+
+
+
+</div>
+</div>
+<div class="form-item form-type-checkboxes form-item-subscribe-author">
+  <label for="edit-subscribe-author">Author: </label>
+ <div class="form-checkboxes" id="edit-subscribe-author">
+
+<div class="bpi_left form-item form-type-checkbox form-item-subscribe-author-0">
+ <input type="checkbox" class="form-checkbox" value="0" name="subscribe_author[0]" id="edit-subscribe-author-0">  <label for="edit-subscribe-author-0" class="option">Peter </label>
+
+</div>
+
+<div class="bpi_left form-item form-type-checkbox form-item-subscribe-audience-0">
+ <input type="checkbox" class="form-checkbox" value="0" name="subscribe_audience[0]" id="edit-subscribe-audience-0">  <label for="edit-subscribe-audience-0" class="option">Auto update </label>
+</div>
+
+<div class="clear"></div>
+
+
+<div class="bpi_left form-item form-type-checkbox form-item-subscribe-author-1">
+ <input type="checkbox" class="form-checkbox" value="1" name="subscribe_author[1]" id="edit-subscribe-author-1">  <label for="edit-subscribe-author-1" class="option">Jan </label>
+
+</div>
+
+<div class="bpi_left form-item form-type-checkbox form-item-subscribe-audience-0">
+ <input type="checkbox" class="form-checkbox" value="0" name="subscribe_audience[0]" id="edit-subscribe-audience-0">  <label for="edit-subscribe-audience-0" class="option">Auto update </label>
+</div>
+
+<div class="clear"></div>
+
+<div class="bpi_left form-item form-type-checkbox form-item-subscribe-author-2">
+ <input type="checkbox" class="form-checkbox" value="2" name="subscribe_author[2]" id="edit-subscribe-author-2">  <label for="edit-subscribe-author-2" class="option">Batman </label>
+
+</div>
+
+<div class="bpi_left form-item form-type-checkbox form-item-subscribe-audience-0">
+ <input type="checkbox" class="form-checkbox" value="0" name="subscribe_audience[0]" id="edit-subscribe-audience-0">  <label for="edit-subscribe-audience-0" class="option">Auto update </label>
+</div>
+
+<div class="clear"></div>
+</div>
+</div>
+<div class="form-item form-type-checkboxes form-item-subscribe-library">
+  <label for="edit-subscribe-library">Library: </label>
+ <div class="form-checkboxes" id="edit-subscribe-library">
+
+<div class="bpi_left form-item form-type-checkbox form-item-subscribe-library-0">
+ <input type="checkbox" class="form-checkbox" value="0" name="subscribe_library[0]" id="edit-subscribe-library-0">  <label for="edit-subscribe-library-0" class="option">Library A </label>
+
+</div>
+<div class="bpi_left form-item form-type-checkbox form-item-subscribe-audience-0">
+ <input type="checkbox" class="form-checkbox" value="0" name="subscribe_audience[0]" id="edit-subscribe-audience-0">  <label for="edit-subscribe-audience-0" class="option">Auto update </label>
+</div>
+<div class="clear"></div>
+
+<div class="bpi_left form-item form-type-checkbox form-item-subscribe-library-1">
+ <input type="checkbox" class="form-checkbox" value="1" name="subscribe_library[1]" id="edit-subscribe-library-1">  <label for="edit-subscribe-library-1" class="option">Library B </label>
+
+</div>
+<div class="bpi_left form-item form-type-checkbox form-item-subscribe-audience-0">
+ <input type="checkbox" class="form-checkbox" value="0" name="subscribe_audience[0]" id="edit-subscribe-audience-0">  <label for="edit-subscribe-audience-0" class="option">Auto update </label>
+</div>
+<div class="clear"></div>
+
+<div class="bpi_left form-item form-type-checkbox form-item-subscribe-library-2">
+ <input type="checkbox" class="form-checkbox" value="2" name="subscribe_library[2]" id="edit-subscribe-library-2">  <label for="edit-subscribe-library-2" class="option">Library C </label>
+
+</div>
+<div class="bpi_left form-item form-type-checkbox form-item-subscribe-audience-0">
+ <input type="checkbox" class="form-checkbox" value="0" name="subscribe_audience[0]" id="edit-subscribe-audience-0">  <label for="edit-subscribe-audience-0" class="option">Auto update </label>
+</div>
+
+<div class="clear"></div>
+</div>
+</div>
+<input type="submit" class="form-submit" value="Submit" name="op" id="edit-submit"><input type="hidden" value="form-hHakjHbQqdvg7hRk1zZhaZAYgwLh5UxDGRsYcNSoVSU" name="form_build_id">
+<input type="hidden" value="1ep6wXt9cdHBlTVeLlP1ZB7DSgeB73RqpNQ7KAvG-6E" name="form_token">
+<input type="hidden" value="bpi_subscribe_form" name="form_id">
+</div>
